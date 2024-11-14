298d527ed2a52b2e3aee172be4d95684d65ae0d8
federicoReghini <federico.reghini@beije.it>
2024-11-14 10:47:02 +0100

    feat(nix): add nix-homebrew and update packages
    - Added nix-homebrew to the flake configuration
    - Updated system packages to include various applications
    - Enabled Homebrew with specific brews```gitcommit
    feat(nix): add nix-homebrew and update packages
    
    - Added nix-homebrew to flake.nix configuration
    - and casks
    - Configured fonts and system activation scripts
    - Updated WezTerm font to JetBrains Mono

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,16 +5,76 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
+
+    nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [
+        pkgs.aerospace
+        pkgs.discord
+        pkgs.mkalias
+        pkgs.neovim
+        pkgs.nushell
+        pkgs.obsidian
+        pkgs.postman
+        pkgs.skhd
+        pkgs.spotify
+        pkgs.tmux
+        pkgs.wezterm
         ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "eza"
+          "fzf"
+          "git"
+          "mas"
+          "nushell"
+          "starship" 
+          "zoxide"
+        ];
+        casks = [
+        # "aerospace"
+        "firefox"
+        "hammerspoon"
+        "wezterm"
+        # "iina"
+        "the-unarchiver"
+        ];
+        onActivation.cleanup = "zap";
+      };
+
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })  
+      ];
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -25,6 +85,7 @@
 
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
+      # programs.nushell.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -41,7 +102,23 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+      configuration
+      nix-homebrew.darwinModules.nix-homebrew
+      {
+        nix-homebrew = {
+          enable = true;
+          # Apple Silicon Only
+          enableRosetta = true;
+
+          # User owning the Homebrew prefix
+          user = "federicoreghini";
+
+          # Automatically migrate existing Homebrew installations
+          autoMigrate = true;
+      };
+      }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
